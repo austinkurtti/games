d00a5d7546f4c975889f3a0dc0ead2934d8939ec
Austin Kurtti <austin.kurtti@gmail.com>
2026-06-08 17:59:58 -0500

    Not found page

--- src/app/features/not-found/not-found.component.ts
+++ src/app/features/not-found/not-found.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NgxbButtonDirective, NgxbButtonType } from '@austinkurtti/ngx-burst/button';
 
 @Component({
     selector: 'pbg-not-found',
     styleUrl: './not-found.component.scss',
-    templateUrl: './not-found.component.html'
+    templateUrl: './not-found.component.html',
+    host: {
+        'class': 'full-page'
+    },
+    imports: [
+        NgxbButtonDirective,
+        RouterLink
+    ]
 })
-export class PbgNotFoundComponent {}
+export class PbgNotFoundComponent {
+    public buttonType = NgxbButtonType.primary;
+}
