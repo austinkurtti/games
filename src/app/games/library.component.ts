7c258221ce182b51e21ff5309985d9178f012afc
Austin Kurtti <austin.kurtti@gmail.com>
2026-06-11 22:02:09 -0500

    Library progress

--- src/app/games/library.component.ts
+++ src/app/games/library.component.ts
@@ -1,6 +1,13 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, inject, OnDestroy, Renderer2 } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { NgxbButtonDirective, NgxbButtonType } from '@austinkurtti/ngx-burst/button';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+
+class Game {
+    public title = '';
+    public route = '';
+    public img = '';
+}
 
 @Component({
     selector: 'pbg-library',
@@ -8,9 +15,37 @@ import { FaIconComponent } from '@fortawesome/angular-fontawesome';
     templateUrl: './library.component.html',
     imports: [
         FaIconComponent,
-        NgxbButtonDirective
+        NgxbButtonDirective,
+        RouterLink
     ]
 })
-export class PbgLibraryComponent {
+export class PbgLibraryComponent implements AfterViewInit, OnDestroy {
     public themeButtonType = NgxbButtonType.subtle;
+    public games: Game[] = [
+        { title: 'Sudoku', route: 'sudoku', img: 'sudoku.png' },
+        { title: 'Warships', route: 'warships', img: 'warships.png' }
+    ];
+
+    private _renderer = inject(Renderer2);
+
+    private _scrolled = false;
+    private _unlistenScroll: (() => void) | null = null;
+
+    public ngAfterViewInit(): void {
+        this._unlistenScroll = this._renderer.listen('window', 'scroll', () => {
+            console.log('scroll listen');
+            const header = document.getElementsByTagName('header')[0];
+            if (!this._scrolled && window.scrollY > 0) {
+                this._scrolled = true;
+                this._renderer.addClass(header, 'scrolling');
+            } else if (this._scrolled && window.scrollY === 0) {
+                this._scrolled = false;
+                this._renderer.removeClass(header, 'scrolling');
+            }
+        });
+    }
+
+    public ngOnDestroy(): void {
+        this._unlistenScroll?.();
+    }
 }
